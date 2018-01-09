c362c02b30f9f7cf3b64a418c35d53a05201451e
Unknown <paul86677889@gmail.com>
2018-01-09 17:51:49 +0800

    change

--- module.js
+++ module.js
@@ -10,28 +10,28 @@
 		this.option = options;
 		this.option2 = options2;
 		this.$Html= $('<img class="mainPic" src="http://fakeimg.pl/600x400/?text=Hello" alt="">'
-					+'<div class="up" src="http://fakeimg.pl/350x200/?text=Hello" alt="">'
-					+'<a class="up-left" href="#"><</a>'
-					+'<div class="upcontent"></div>'
-					+'<a class="up-right" href="#">></a>'
-					+'</div>'
-					+'<div class="img-down">'
-					+'<div class="img-down_left">※客房以實際入住房型為主,圖片僅供參考</div>'
-					+'<div class="img-down_right">查看大圖<img src="./zoomIn.png" alt=""></div>'
-					+'</div>'
+						+'<div class="up" src="http://fakeimg.pl/350x200/?text=Hello" alt="">'
+						+'<a class="up-left" href="#"><</a>'
+						+'<div class="upcontent"></div>'
+						+'<a class="up-right" href="#">></a>'
+						+'</div>'
+						+'<div class="img-down">'
+						+'<div class="img-down_left">※客房以實際入住房型為主,圖片僅供參考</div>'
+						+'<div class="img-down_right">查看大圖<img src="./zoomIn.png" alt=""></div>'
+						+'</div>'
 					);
 		this.$Html2=$('<div class="down">'
-					+'<a class="down-left"> < </a>'
-					+'<ul>'
-					+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
-					+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
-					+'<li><img class="smallPic" src="http://fakeimg.pl/250x250/"></li>'
-					+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
-					+'<li><img class="smallPic" src="http://fakeimg.pl/350x200/"></li>'
-					+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
-					+'</ul>'
-					+'<a class="down-right"> > </a>'
-					+'</div>')
+						+'<a class="down-left"> < </a>'
+						+'<ul>'
+						+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
+						+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
+						+'<li><img class="smallPic" src="http://fakeimg.pl/250x250/"></li>'
+						+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
+						+'<li><img class="smallPic" src="http://fakeimg.pl/350x200/"></li>'
+						+'<li><img class="smallPic" src="http://fakeimg.pl/350x300/"></li>'
+						+'</ul>'
+						+'<a class="down-right"> > </a>'
+						+'</div>')
 
 	};
 	// 下面是DEFAULTS物件 
@@ -58,6 +58,8 @@
 	
 //這裡有clearTimer()
 	Module.prototype.init = function () {
+		var a = this.option.pageSize;
+		console.log(a);
 		this.creatHtml();
 		$(".smallPic").click(function(){
 			var smallPicSrc =this.src;//這裡的this指向觸發click事件的物件
